e288d4dff5527375803c82e70940e4a8521e3c4a
Loading… <70993217+zdisanto@users.noreply.github.com>
2023-11-02 13:16:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+  alert(a + b);
+}
+function feeling() {
+  document.getElementById("feel").innerHTML = "I'm Feeling Goofy";
+}
+function tryMe() {
+  let huh = "Watch it! What do you think I am, Google?";
+  alert(huh);
+}
